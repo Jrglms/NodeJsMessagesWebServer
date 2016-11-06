1db99b767c0bf72381283cd3018b439cb6b233c8
Jorge Lomas <jorge.lomas.rodriguez+dev@gmail.com>
2016-11-06 19:55:08 +0000

    Messages could now be created
    But we need to let our server parse the url encoded forms.

--- webServer/controllers/messagesController.js
+++ webServer/controllers/messagesController.js
@@ -5,8 +5,39 @@
     messagesController.init = function (app) {
 
         var http = require("http");
+        
+        app.post("/messages", function (req, res) {
+
+            var request = require("request");
+
+            request({
+                uri: "/messages",
+                baseUrl: baseUrl,
+                method: "POST",
+                json: true,
+                headers: {
+                    "user-identifier": "1000"
+                },
+                body: { message: req.body.message }
+            }, function (err, response) {
+
+                if (err) {
+                    console.log(err);
+
+                    res.status(500);
+                }
+                else {
+                    returnGlobalMessages(req, res);
+                }
+            });
+        });
 
         app.get("/messages", function (req, res) {
+
+            returnGlobalMessages(req, res);
+        });
+
+        var returnGlobalMessages = function (req, res) {
 
             var request = require("request");
 
@@ -29,7 +60,7 @@
                     res.render("messages/globalMessages", { messages: body });
                 }
             });
-        });
+        }
     };
 
 })(module.exports)
