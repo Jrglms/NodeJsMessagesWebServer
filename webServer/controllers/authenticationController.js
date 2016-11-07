cc0183477b69494465880f6f377a094492ba5f2e
Jorge Lomas <jorge.lomas.rodriguez+dev@gmail.com>
2016-11-07 19:34:03 +0000

    Added authentication to web server

---
+++ webServer/controllers/authenticationController.js
@@ -0,0 +1,20 @@
+﻿(function (authenticationController) {
+
+    authenticationController.init = function (app) {
+
+        app.all("*", function (req, res, next) {
+
+            if (req.session.userIdentifier) {
+                next();
+            }
+            else if (req.body.userIdentifier) {
+                req.session.userIdentifier = req.body.userIdentifier;
+                next();
+            }
+            else {
+                res.render("login");
+            }
+        })
+    };
+
+})(module.exports)
